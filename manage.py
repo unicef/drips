d364d844da5b02b74cd9accdff0b97571e164feb
Domenico DiNicola <dom.dinicola@gmail.com>
2023-04-04 15:56:44 +0200

    trigger

--- manage.py
+++ manage.py
@@ -4,12 +4,11 @@ import sys
 
 SRC = os.path.abspath("src")
 sys.path.insert(0, SRC)
-if sys.argv[1] == 'test':
+if sys.argv[1] == "test":
     os.chdir(os.path.join(SRC, "drips"))
 
 
 if __name__ == "__main__":
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drips.config.settings")
 
     from django.core.management import execute_from_command_line
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from codecs import open
 from setuptools import find_packages, setup
 
 ROOT = os.path.realpath(os.path.dirname(__file__))
-init = os.path.join(ROOT, 'src', 'drips', '__init__.py')
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-_name_re = re.compile(r'NAME\s+=\s+(.*)')
+init = os.path.join(ROOT, "src", "drips", "__init__.py")
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+_name_re = re.compile(r"NAME\s+=\s+(.*)")
 
-sys.path.insert(0, os.path.join(ROOT, 'src'))
+sys.path.insert(0, os.path.join(ROOT, "src"))
 
-with open(init, 'rb') as f:
-    content = f.read().decode('utf-8')
+with open(init, "rb") as f:
+    content = f.read().decode("utf-8")
     VERSION = str(ast.literal_eval(_version_re.search(content).group(1)))
     NAME = str(ast.literal_eval(_name_re.search(content).group(1)))
 
@@ -24,23 +24,23 @@ dependency_links = set()
 setup(
     name=NAME,
     version=VERSION,
-    author='UNICEF',
-    author_email='drips@unicef.org',
-    url='',
-    description='',
-    long_description=open(os.path.join(ROOT, 'README.rst')).read(),
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
+    author="UNICEF",
+    author_email="drips@unicef.org",
+    url="",
+    description="",
+    long_description=open(os.path.join(ROOT, "README.rst")).read(),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     zip_safe=False,
     dependency_links=list(dependency_links),
-    license='BSD',
+    license="BSD",
     include_package_data=True,
     classifiers=[
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.11',
-        'Framework :: Django',
-        'Framework :: Django :: 4.1',
-        'Framework :: Flake8',
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Flake8",
     ],
 )
